4b6479d38e0994d8d9149745c90e77dea12c05f6
mayanyan1991 <mayan19911114@gmail.com>
2022-09-14 18:07:41 +0200

    save new character into the text file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import pandas as pd
 
 def search_vol(list_char, list_dic):
+    #search for all the vocabularies in list_dic contains 
+    # only characters in the list_char
     list_learned_vol = []
     for char in list_char: 
         for vol in list_dic:
@@ -27,6 +29,7 @@ for key in keys:
 with open('learned.txt','r',encoding='utf-8') as f:
     learned_list = f.read()
 learned_list = learned_list.split('\n')
+
 #imput of new characters from users
 
 new_char = input('Enter new characters you \
@@ -34,6 +37,7 @@ new_char = input('Enter new characters you \
 
 # append the new characters into list if not in the old list
 learned_list.extend(new_char.split(' '))
+learned_list = list(dict.fromkeys(learned_list))
 
 # search for learned words in dictionary
 vol_dic_learned = {}
@@ -43,3 +47,6 @@ for level in keys:
     vol_dic_learned[level]= search_vol(learned_list, vol_dic[level]['simp_chinese'].values)
     vol_dic_full_learned[level] = vol_dic[level][vol_dic[level]['simp_chinese'].isin(vol_dic_learned[level])]
     
+# add the new character into text file
+with open('learned.txt','w',encoding='utf-8') as f:
+    f.write('\n'.join(learned_list))
